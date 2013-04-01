3cb6aea5d2f06d6087734b8d3ac62b9c9d3c6760
Adam Kochanowicz <ajkochanowicz@gmail.com>
2013-04-01 13:35:48 -0400

    gitignored gh pages

--- dist/kickstrap/apps/ember/config.js
+++
@@ -1,5 +0,0 @@
-define(['apps/ember/handlebars'], function() {
-	define(['apps/ember/handlebars','apps/ember/ember'], function() {}, function(err) { console.kbash(err) })
-})
-
-// TODO: RJS doesn't like this.
