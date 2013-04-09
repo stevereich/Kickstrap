76a0f71a50937b07e3effd5ada6593ba794a54d0
Adam Kochanowicz <ajkochanowicz@gmail.com>
2013-04-09 15:57:26 -0400

    improvements to messages

--- dist/kickstrap/_examples/assets/messages.js
+++ dist/kickstrap/_examples/assets/messages.js
@@ -1,65 +1,118 @@
 var allThreads = [ 
    {
       threadId: 1 
+      , read: false
       , subject: 'Hey, what\'s up?'
       , messages: [ 
          {
-            contactId: 000000005
+            contactId: 5
             , text: 'Hey, just wanted to say hi.'
          }
          , {
-            contactId: 000000004
+            contactId: 4
             , text: 'Hey! How are you?'
          }
          , {
-            contactId: 000000005
+            contactId: 5
             , text: 'Sorry I broke your skateboard :/.'
          }
          , {
-            contactId: 000000004
+            contactId: 4
             , text: 'Meh. ;) It was a cool ollie.'
          }
          , {
-            contactId: 000000005
+            contactId: 5
             , text: 'Ha ha. Very true!'
          }
          , {
-            contactId: 000000004
+            contactId: 4
             , text: 'So what\'s going on this weekend?'
          }
          , {
-            contactId: 000000005
+            contactId: 5
             , text: 'I have to run errands tomorrow, but I\'ll be free on Sunday. You?'
          }
          , {
-            contactId: 000000004
+            contactId: 4
             , text: 'I\'m pretty much free all weekend, but I do need to pump Iron at some point.'
          }
          , {
-            contactId: 000000005
+            contactId: 5
             , text: 'That is so lame. Do you really call it "pumping iron?"'
          }
       ]
    }
    , {
       threadId: 2 
+      , read: true
       , subject: 'Hey there'
       , messages: [ 
          {
-            contactId: 000000002
+            contactId: 2
             , text: 'Did you get that thing I sent you?'
          }
          , {
-            contactId: 000000001
+            contactId: 1
             , text: 'Yes, thank you.'
          }
          , {
-            contactId: 000000002
+            contactId: 2
             , text: 'And it opened okay?.'
          }
          , {
-            contactId: 000000001
+            contactId: 1
             , text: 'I had to convert it to a different file format, but yeah. It\'s all good.'
+         }
+      ]
+   }
+   , {
+      threadId: 3
+      , read: true
+      , subject: 'Can you get those TPS reports over to me?'
+      , messages: [
+         {
+            contactId: 3
+            , text: 'Hey there champ. I was just looking over your schedule today and noticed you didn\'t get your TPS reports in with the new cover?'
+         }
+         , {
+            contactId: 6
+            , text: 'Yeah. You know...you\'re right. I just--I forgot to get the new cover on them. I do apologize'
+         }
+         , {
+            contactId: 3
+            , text: 'Right. right. There was a memo that went out asking everyone to put the new covers on those TPS reports. Did you maybe not get that memo?'
+         }
+         , {
+            contactId: 6
+            , text: 'No, actually I did--I just--it slipped my mind. So I will have the new cover on the reports for now on.'
+         }
+         , {
+            contactId: 3
+            , text: 'Sounds great. Oh, and by the way, you know what tomorrow is.'
+         }
+         , {
+            contactId: 6
+            , text: 'I do?'
+         }
+         , {
+            contactId: 3
+            , text: 'Tomorrow is Hawaiian t-shirt day.'
+         }
+         , { 
+            contactId: 6
+            , text: 'Right...'
+         }
+         , {
+            contactId: 3
+            , text: 'So you know. Where a Hawaiian t-shirt...if you want to.'
+         }
+         , {
+            contactId: 6
+            , text: 'Yep, I\'ll see what I can do.'
+         }
+         , {
+            contactId: 3
+            , text: 'Greaaaat. Thanks, champ.'
          }
       ]
    }
