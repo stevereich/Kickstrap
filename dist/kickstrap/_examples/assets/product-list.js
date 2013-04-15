79e1bc05b30d99ee99973a57a0773a8ef69738d8
Adam Kochanowicz <ajkochanowicz@gmail.com>
2013-04-15 11:25:41 -0400

    finishing dummy data and making products and contacts sort.

--- dist/kickstrap/_examples/assets/product-list.js
+++ dist/kickstrap/_examples/assets/product-list.js
@@ -1,13 +1,16 @@
+var sortFunction = function(a, b) {
+   return a.title < b.title ? -1 : 1;
+}
 function productListViewModel() {
 	this.products = ko.observableArray(allProducts)
 	this.filterTerm = ko.observable()
 
 	this.filteredItems = ko.computed(function() {
 		var term = this.filterTerm()
-		if (!term) { return this.products() }
+		if (!term) { return this.products.sort(sortFunction) }
 		return ko.utils.arrayFilter(this.products(), function(item) {
 			return item.title.toLowerCase().indexOf(term.toLowerCase()) > -1 
-      });
+      }).sort();
 	}, this)
 } 
 
