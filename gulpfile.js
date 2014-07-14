f47c9c6106f2575e66da28eabb26ce0910aa7c3e
Adam Kochanowicz <ajkochanowicz@gmail.com>
2014-07-14 16:03:06 -0400

    Ripping out more unneeded stuff

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var gulp = require('gulp'),
+  sass = require('gulp-ruby-sass'),
+  watch = require('gulp-watch'),
+  connect = require('gulp-connect'),
+  jade = require('gulp-jade');
+
+
+// Main entry point
+
+gulp.task('default', ['connect', 'watch']);
+
+// Basic builder
+
+gulp.task('build', function() {
+  gulp.start('compile:css', 'compile:jade');
+})
+
+// Compilers
+
+gulp.task('compile:css', function() {
+  return gulp.src(['./lib/sass/**/*.{sass, scss}'])
+    .pipe(sass())
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('compile:jade', function() {
+  return gulp.src(['./lib/jade/**/*.jade'])
+    .pipe(jade())
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('connect', function() {
+  connect.server({
+    livereload: true
+  });
+});
+
+gulp.task('watch' , function() {
+  gulp.watch(['./lib/sass/**/*.{sass, scss}'], ["compile:css"]);
+  gulp.watch(['./lib/jade/**/*.jade'], ["compile:jade"]);
+});
